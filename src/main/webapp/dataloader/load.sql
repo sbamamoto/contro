1d99a505c3bb696286520fca9342e06d2a97d6e2
Barmeier <matthias.barmeier@sourcepark.de>
2020-11-02 18:24:53 +0100

    Adding more script features

--- src/main/webapp/dataloader/load.sql
+++ src/main/webapp/dataloader/load.sql
@@ -1,6 +1,6 @@
 insert into processor (id,version,name,description,type,processing_script) values (0,0,'ArduControl','Arduino Gateway Switching','SCRIPT','def groovy');
 insert into processor (id,version,name,description,type,processing_script) values (1,0,'ArduWifi','"Arduino WLAN Switching"','SCRIPT','def groovy');
-insert into processor (id,version,name,description,type,processing_script) values (2,0,'HomeMatic','"Homematic Bridge Interface"','SCRIPT','def groovy');
+insert into processor (id,version,name,description,type,processing_script) values (2,0,'HomeMatic','"Homematic Bridge Interface"','SCRIPT','fqaddr = address+channel\ncommand = "SET_POINT_TEMPERATURE"\ncontro.SwitchService.httpSwitch("http://localhost:9998/hapi/setvalue?address="\n+ fqaddr + "&key=" + command + "&value=" + value)');
 
 insert into ability (id,version,name,description,type,read_only, processor_id) values (0,0,'WifiControl','Arduino Gateway Switch','DIRECT',false, 0);
 insert into ability (id,version,name,description,type,read_only, processor_id) values (1,0,'Switch ArduControl','Arduino WLAN Switch','DIRECT',false, 1);
@@ -9,5 +9,10 @@ insert into ability (id,version,name,description,type,read_only, processor_id) v
 insert into device_type (id,version,name,description) values (0,0,'Thermostat (HMCIP)','Homematic IP Thermostat');
 insert into device_type (id,version,name,description) values (1,0,'Singleswitch','ArduControl 433MHz Schalter');
 insert into device_type (id,version,name,description) values (2,0,'Multiswitch','ArduWifi Schalter mit Kanalwahl');
+
+insert into interface (id,version,url,description,type) values (0,0,'http://<xyour gateway address>/','ArduControl Gateway','433');
+insert into interface (id,version,url,description,type) values (1,0,'http://<xyour gateway address>/','Homematic Bridge URL','HOMEMATIC');
+insert into interface (id,version,url,description,type) values (2,0,'http://<xyour gateway address>/','ArduWifi SwitchAddress','ARDUWIFI');
+
 
 insert into setting (id,version, setting, value) values (0,0,'partymode', 'OFF');
---
+++ src/test/groovy/contro/HomematicControllerSpec.groovy
@@ -0,0 +1,18 @@
+package contro
+
+import grails.testing.web.controllers.ControllerUnitTest
+import spock.lang.Specification
+
+class HomematicControllerSpec extends Specification implements ControllerUnitTest<HomematicController> {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+        expect:"fix me"
+            true == false
+    }
+}
